83339a0a6f61ce21a5fa7d088ce2ca25dddaa14f
raffiabyann <raffizizou4@gmail.com>
2025-09-10 20:08:06 +0700

    First Commit Week3

---
+++ src/week03/raffi/id/umn/ac/Week03.java
@@ -0,0 +1,56 @@
+package week03.raffi.id.umn.ac;
+
+import java.util.Scanner;
+public class Week03 {
+
+	public static void main(String[] args) {
+		 int input;
+	        Scanner scanner = new Scanner(System.in);
+
+	        String[] matkulIF = {
+	            "Matematika Diskrit",
+	            "Dasar-dasar Pemrograman",
+	            "Pemrogramman Berorientasi Objek"
+	        };
+
+	        String[] matkulCE = {
+	            "Riset Operasi",
+	            "Jaringan Komputer",
+	            "Aljabar Linear"
+	        };
+
+	        String[] matkulIS = {
+	            "Sistem Database",
+	            "Administrasi Database"
+	        };
+
+	        System.out.println("Pilih kategori mata kuliah :");
+	        System.out.println("1. Informatics\n2. Computer Engineering\n3. Information System");
+	        System.out.print("Pilih : ");
+	        input = scanner.nextInt();
+
+	        switch(input) {
+	            case 1: 
+	                System.out.println("Daftar mata kuliah IF : ");
+	                show(matkulIF); 
+	                break;
+	            case 2: 
+	                System.out.println("Daftar mata kuliah CE : ");
+	                show(matkulCE); 
+	                break;
+	            case 3: 
+	                System.out.println("Daftar mata kuliah IS : ");
+	                show(matkulIS); 
+	                break;
+	            default: 
+	                System.out.println("Pilihan tidak valid");
+	        }
+	    }
+
+	    public static void show(String[] matkul) {
+	        for(String eachMatkul : matkul) {
+	            System.out.println("- " + eachMatkul);
+	        }
+	    }
+	}
+
